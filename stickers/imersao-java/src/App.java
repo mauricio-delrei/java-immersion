94ca8a92681a99610565ec85642707f6a038f905
Mauricio Del Rei <mauricioaraujodelrei@gmail.com>
2023-03-29 23:12:17 +0100

    Generating Stickers for Whatsapp

--- stickers/imersao-java/src/App.java
+++ stickers/imersao-java/src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -26,12 +28,20 @@ public class App {
 
       
 
-        // exibir e manipular os dados 
-        for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
-            System.out.println();
-        }
+         // exibir e manipular os dados 
+         var geradora = new GeradoraDeFigurinhas();
+         for (Map<String,String> filme : listaDeFilmes) {
+ 
+             String urlImagem = filme.get("image");
+             String titulo = filme.get("title");
+ 
+             InputStream inputStream = new URL(urlImagem).openStream();
+             String nomeArquivo = titulo + ".png";
+ 
+             geradora.cria(inputStream, nomeArquivo);
+ 
+             System.out.println(titulo);
+             System.out.println();
     }
+}
 }
